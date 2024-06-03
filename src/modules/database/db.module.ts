53655bf2095ca221c7e67c03103ad4c17ab4c57a
khanhtranicd <khanhtc@icd-vn.com>
2024-06-03 13:51:05 +0700

    use env from configmap

--- src/modules/database/db.module.ts
+++ src/modules/database/db.module.ts
@@ -4,12 +4,12 @@ import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOpti
 
 const connectionConfig: MysqlConnectionOptions = {
   type: 'mysql',
-  host: process.env.MYSQL_HOST || '127.0.0.1',
+  host: process.env.MYSQL_HOST || '34.84.9.234',
   port: Number(process.env.MYSQL_PORT) || 3306,
-  username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'password',
-  database: process.env.MYSQL_DB || 'nest-base',
-  extra: process.env.CLOUD_SQL_INSTANT_NAME ? { socketPath: process.env.CLOUD_SQL_INSTANT_NAME } : null,
+  username: process.env.MYSQL_USER || 'nestjs-10-user',
+  password: process.env.MYSQL_PASSWORD || 'Qr>JgAFjj0?K5RFG',
+  database: process.env.MYSQL_DB || 'nestjs-10',
+  // extra: process.env.CLOUD_SQL_INSTANT_NAME ? { socketPath: process.env.CLOUD_SQL_INSTANT_NAME } : null,
   synchronize: true,
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   subscribers: [],
