1a0c4d686c52cca015b4948b260fea5bced4e6c1
khanhtranicd <khanhtc@icd-vn.com>
2024-06-04 09:07:23 +0700

    use env from configmap

--- src/modules/database/db.module.ts
+++ src/modules/database/db.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
+// import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
 
-const connectionConfig: MysqlConnectionOptions = {
+const connectionConfig: any = {
   type: 'mysql',
   host: process.env.MYSQL_HOST || '127.0.0.1',
   port: Number(process.env.MYSQL_PORT) || 3306,
-  username: process.env.MYSQL_USER || 'nestjs-10-user',
-  password: process.env.MYSQL_PASSWORD || 'Qr>JgAFjj0?K5RFG',
+  user: process.env.MYSQL_USER || 'nestjs-10-user',
+  password: process.env.MYSQL_PASSWORD|| 'Qr>JgAFjj0?K5RFG',
   database: process.env.MYSQL_DB || 'nestjs-10',
   extra: process.env.CLOUD_SQL_INSTANT_NAME || 'ceremonial-team-424503-u1:asia-northeast1:nestjs-10-sql',
   synchronize: true,
