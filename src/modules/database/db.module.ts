b4d4a1219872c9d9a21778c8bb04f90d3a81913e
khanhtranicd <khanhtc@icd-vn.com>
2024-06-04 09:42:00 +0700

    use env from configmap

--- src/modules/database/db.module.ts
+++ src/modules/database/db.module.ts
@@ -12,7 +12,7 @@ const connectionConfig: MysqlConnectionOptions = {
   // extra: process.env.CLOUD_SQL_INSTANT_NAME || 'ceremonial-team-424503-u1:asia-northeast1:nestjs-10-sql',
   synchronize: true,
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-  // subscribers: [],
+  subscribers: [],
 };
 @Module({
   imports: [
