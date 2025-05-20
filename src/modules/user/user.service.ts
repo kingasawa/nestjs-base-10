8560b715c241f9836827fae293041d06cace9e29
khanhtranicd <khanhtc@icd-vn.com>
2025-05-20 08:25:34 +0700

    trace logs

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,6 +1,25 @@
 import { Injectable } from '@nestjs/common';
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { ConfigService } from '@nestjs/config';
+const API_KEY = this.configService.get('OPENAI_API_KEY');
+const axiosConfig: AxiosRequestConfig = {
+  baseURL: "https://api.openai.com/v1",
+  timeout: 30000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+}
+console.log('API_KEY', API_KEY);
+const api = axios.create(axiosConfig);
+api.interceptors.request.use(
+  async (config) => {
+    config.headers.Authorization = `Bearer ${API_KEY}`;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 @Injectable()
 export class UserService {
@@ -11,20 +30,13 @@ export class UserService {
   public async talkToBot(payload: any): Promise<any> {
     console.log('payload', payload);
     const { conversation } = payload;
-    const API_KEY = this.configService.get('OPENAI_API_KEY');
 
     try {
       const response = await axios.post(
-        'https://api.openai.com/v1/chat/completions',
+        '/chat/completions',
         {
           model: 'gpt-4-turbo',
           messages: conversation,
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${API_KEY}`,
-            'Content-Type': 'application/json',
-          },
         },
       );
       console.log('response.data.choices[0]', response.data.choices[0]);
